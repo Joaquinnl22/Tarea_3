e617e2272395ccb31571b6d38a2c8a0959741b61
Joaquinl <96803068+Joaquinnl22@users.noreply.github.com>
2022-06-26 23:50:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const cassandra = require('cassandra-driver');
 
 client = new cassandra.Client({
-  contactPoints: [process.env.CONTACT_POINTS],
-  localDataCenter: process.env.LOCAL_DATACENTER,
+  contactPoints: ['cassandranodo1'],
+  localDataCenter: 'cassandra-cluster',
   credentials: { username: process.env.CASSANDRA_USERNAME, password: process.env.CASSANDRA_PASSWORD},
 });
