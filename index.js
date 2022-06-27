5ae966530ecbcd651dc8912b871711f85c6eae80
Joaquinl <96803068+Joaquinnl22@users.noreply.github.com>
2022-06-27 00:00:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ client = new cassandra.Client({
   localDataCenter: 'cassandra-cluster',
   credentials: { username: process.env.CASSANDRA_USERNAME, password: process.env.CASSANDRA_PASSWORD},
 });
+
+async function insertPaciente(nombre, apellido,rut, email, fecha_nacimiento){
+    const query = `SELECT MAX(id) AS max_id FROM ks1.paciente;`;
+    const pacientes = await client.execute(query);
+    const new_paciente = paciente.rows[0].max_id + 1;
+    const query_I = `INSERT INTO ks1.paciente(id,nombre, apellido, rut, email, fecha_nacimiento)
+                          VALUES (${new_paciente},'${nombre}','${apellido}','${rut}','${email}','${fecha_nacimiento}');`;
+  await client.execute(query_I);
+  return(new_pasiente)
+}
